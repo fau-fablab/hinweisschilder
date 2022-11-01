009d860036783dafa1ed10156c511eefe2bd0bfd
Harald Kraft <harald.kraft@fau.de>
2022-11-01 18:08:18 +0100

    completely redid the qr code generation script since the lib used in the old one was incompatible with python 3

--- generate-qr.py
+++ generate-qr.py
@@ -1,39 +1,54 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (C) Max
+# (Copyleft) Harald
 
-import PyQRNative
+import os, pyqrcode, qrcode
+from qrcode.image.styledpil import StyledPilImage
 
-def makeQR(text,dest):
-	size=3
-	# this function mostly taken from http://www.blog.pythonlibrary.org/2012/05/18/creating-qr-codes-with-python/
-	for size in range(1,5):
-		try:
-			qr = PyQRNative.QRCode(size, PyQRNative.QRErrorCorrectLevel.M)
-			qr.addData(text)
-			qr.make()
-		except TypeError:
-			# erhöhe size, weil URL zu lang für gewählte size
-			if size==5:
-				raise Exception("URL zu lang - size in makeQR erhöhen oder besser URL kürzen")
-			continue
-		break
-	im = qr.makeImage()
-	img_file = open(dest, 'wb')
-	im.save(img_file, 'PNG')
-	img_file.close()
+fablabMainURL = "https://fablab.fau.de/"
+
+# Only place wordpress-permalinks in this list. Otherwise, links will break since machines are in (sub-)groups now.
+
+maschines = {
+	"fablab",
+	"lasercutter",
+	"multifunktionstisch",
+	"schneideplotter",
+	"textilpresse",
+	"ultraschallbad",
+	"cnc-drehbank",
+	"cnc-fraese",
+	"3d-drucker",
+	"reflow-ofen",
+	"staubsauger",
+	"dremel",
+	"fein-kleinbohrmaschine",
+	"feinschleifer",
+	"oberfraese",
+	"pendelstichsaege",
+	"tauchsaege",
+	"platinenfertigung",
+	"naehmaschine",
+	"oesen-und-druckknopfpresse",
+	"stickmaschine"
+}
+
+def makeQR(toolName, url):
+	print("generating qr code for " + toolName+"...")
+	qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+	qr.add_data(url)
+	img = qr.make_image(image_factory=StyledPilImage, embeded_image_path="./fab_cube.png")
+	img.save(toolName+".png")
 
 
+def main():
+	os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
-import os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-codes = []
-maschinen = ["lasercutter","3d-drucker","schneideplotter","cnc-fraese","cnc-drehbank", "platinenfertigung","3d-drucker-makerbot","cnc-fraese-roland","farblaserdrucker-canon-clc3200","3d-drucker-ultimaker","reflow-ofen","absaugmobil-cleantec-ctm-26","oberfrase-1010","multifunktionstisch-mft-3","tauchsage-ts-55-r"]
-for m in maschinen:
-	codes = codes + [["https://fablab.fau.de/tool/" + m, m]]
-# TODO something like cd (dirname $0) - change to the directory that the script lives inside
-for [text, dest] in codes:
-	makeQR(text, dest+".png")
+	for name in maschines:
+		if name != "fablab":
+			makeQR(name, fablabMainURL + name)
+		else:
+			makeQR(name, fablabMainURL)
 	
+if __name__=="__main__":
+	main()
